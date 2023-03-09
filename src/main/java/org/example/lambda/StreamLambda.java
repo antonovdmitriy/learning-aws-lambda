ad0a6a000b9c3860f2922e2a55592c18f7fc2649
Dmitrii_Antonov <antonov.buscorr@gmail.com>
2023-03-09 17:22:03 +0100

    Add lambda with io streams

---
+++ src/main/java/org/example/lambda/StreamLambda.java
@@ -0,0 +1,18 @@
+package org.example.lambda;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamLambda {
+
+    /**
+     * to test use "my simple text"
+     */
+    public void handlerStream(InputStream inputStream, OutputStream outputStream) throws IOException {
+        int letter;
+        while ((letter = inputStream.read()) != -1) {
+            outputStream.write(Character.toUpperCase(letter));
+        }
+    }
+}
